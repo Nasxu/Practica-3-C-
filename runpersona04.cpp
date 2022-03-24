52f46f6d4e667e143f4f8cd423c3e18d14c3da70
Nasxu <87586585+Nasxu@users.noreply.github.com>
2022-03-24 10:55:10 -0500

    Add files via upload

---
+++ runpersona04.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include "persona.h"
+#include "persona.cpp"
+using namespace std;
+
+int main(){
+    FILE *archivo;
+    archivo = fopen("personas.dat","rb");
+    if (archivo == NULL){
+        cout << "ERROR: EL ARCHIVO NO PUDO ABRIRSE." << endl; cin.get();
+        exit(0);
+    }
+    int numero;
+    cout << "Ingrese el numero de personas en la lista: "; cin >> numero ; cout << endl;
+    Persona *lista = new Persona[numero];
+    for (int i=0; i<numero; i++){
+        lista[i].ingresar();
+    }
+    fwrite(lista, sizeof(Persona),numero,archivo);
+
+    for (int i=0; i<numero; i++){
+        lista[i].imprimir();
+    }
+    fclose(archivo);
+    return 0;
+
+}
